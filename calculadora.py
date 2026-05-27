f4765ef8624d99b2120ecf9a975e280b3288f2f2
Guillem <guillembirds@gmail.com>
2026-05-27 10:41:59 +0200

    sacados los escaños y arreglado codigo muerto en el apartado sumas

--- calculadora.py
+++ calculadora.py
@@ -4,24 +4,28 @@ while True:
     print("2. Restar")
     print("3. Multiplicar")
     print("4. Dividir")
+    print("5. Salir")
     
-    opcion = input("Que quieres hacer (1-4): ")
+    opcion = input("Que quieres hacer (1-5): ")
     
     if opcion == "1":
         while True:
             numero1 = input("Pon el primer numero: ")
             if numero1.isdigit():
                 numero1 = int(numero1)
-                continue
+                break
             else:
                 print("Eso no es un numero... vuelve a probar")
-                
+                continue
+        while True:        
             numero2 = input("Pon el segundo numero: ")
-                if numero2.isdigit():
-                    numero2 = int(numero2)
-                    continue
-                else:
-                    print("Eso no es un numero... vuelve a probar")
-            resultadosum = numero1 + numero2
-            print(f"La suma de {numero1} y {numero2} es: {resultadosum}")
-        exit()
+            if numero2.isdigit():
+                numero2 = int(numero2)
+                break
+            else:
+                print("Eso no es un numero... vuelve a probar")
+                continue
+                
+        resultadosum = numero1 + numero2
+        print(f"La suma de {numero1} y {numero2} es: {resultadosum}")
+exit()
